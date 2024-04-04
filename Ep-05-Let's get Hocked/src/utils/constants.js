ece82b81f24e7f4940812b0d745820d196d2f7e0
Harsh Kumar <harsh.kumar@robomq.io>
2024-04-04 17:38:55 +0530

    folder name uodated

---
+++ Ep-05-Let's get Hocked/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const LOGO_URL = "https://www.creativefabrica.com/wp-content/uploads/2021/04/27/food-ordering-logo-design-online-food-Graphics-11416444-1-580x435.png";
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
