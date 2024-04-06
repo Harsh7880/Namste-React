136acbe408a71eb6c554577adaf68924743a7c55
Harsh Kumar <harsh.kumar@robomq.io>
2024-04-06 18:35:59 +0530

    Added shimmer UI

--- Ep-06-Exploring The World/src/components/Body.js
+++ Ep-06-Exploring The World/src/components/Body.js
@@ -1,41 +1,47 @@
-import RestorantCard from './RestorantCard'
-import {useState, useEffect} from 'react'
-import resList from '../utils/mockData'
-import { API_URL } from '../utils/constants'
+import RestorantCard from "./RestorantCard";
+import { useState, useEffect } from "react";
+import { API_URL } from "../utils/constants";
+import Shimmer from "./Shimmer";
 const Body = () => {
-
-    const [listOfRestorants, setListOfRestorants] = useState([]);
-    useEffect(  ()=> {
+  const [listOfRestorants, setListOfRestorants] = useState([]);
+  useEffect(() => {
     fetchData();
-    },[])
+  }, []);
 
-    const fetchData = async () => {
+  const fetchData = async () => {
     const data = await fetch(API_URL);
     const JsonData = await data.json();
-    console.log(JsonData.data.cards[4].card.card);
-    setListOfRestorants(JsonData.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-    
-    }
-    return (
-        <div className='body'>
-            <div className='search-bar'>
-                <button className='filter-restorant'
-                onClick = {()=>{
-                      const  filteredList = listOfRestorants.filter(
-                            (res) => res.info.avgRating > 4
-                            );
-                            setListOfRestorants(filteredList);
-                    }
-                }
-                > Top Rated Restorant's</button>
-            </div>
-             <div className='res-container'>
-               {
-                listOfRestorants.map( (restaurant) => <RestorantCard key={restaurant.info.id} resData = {restaurant} />)
-                }
-              </div>
-        </div>
-    )
-}
+    setListOfRestorants(
+        // Optional Chaning
+      JsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+// Conditional Rendering
+  
+  return listOfRestorants.length === 0 ? <Shimmer/> :  (
+    <div className="body">
+      <div className="search-bar">
+        <button
+          className="filter-restorant"
+          onClick={() => {
+            const filteredList = listOfRestorants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestorants(filteredList);
+          }}
+        >
+          Top Rated Restorant's
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestorants.map((restaurant) => (
+          <RestorantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
