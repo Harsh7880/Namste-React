6366c7e9ce523f8f56807bef97d35ab63a284693
Harsh Kumar <harsh.kumar@robomq.io>
2024-04-06 21:24:13 +0530

    search functionality Added

--- Ep-06-Exploring The World/src/components/Body.js
+++ Ep-06-Exploring The World/src/components/Body.js
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 import { API_URL } from "../utils/constants";
 import Shimmer from "./Shimmer";
 const Body = () => {
-
   const [listOfRestorants, setListOfRestorants] = useState([]);
-  
+  const [filteredRestorants, setFilteredRestorants] = useState([]);
+
+  const [searchValue, setSearchValue] = useState("");
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -14,17 +16,42 @@ const Body = () => {
     const data = await fetch(API_URL);
     const JsonData = await data.json();
     setListOfRestorants(
-        // Optional Chaning
+      // Optional Chaning
       JsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
+    setFilteredRestorants(JsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants);
   };
 
-// Conditional Rendering
-  
-  return listOfRestorants.length === 0 ? <Shimmer/> :  (
+  // Conditional Rendering
+
+  return listOfRestorants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="search-bar">
+        <div className="search">
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+          />
+          <button
+            className="search"
+            onClick={() => {
+              const filteredRestorants = listOfRestorants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchValue.toLowerCase())
+              );
+              setFilteredRestorants(filteredRestorants);
+            }}
+          >
+            {" "}
+            Search
+          </button>
+        </div>
         <button
           className="filter-restorant"
           onClick={() => {
@@ -38,7 +65,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestorants.map((restaurant) => (
+        {filteredRestorants.map((restaurant) => (
           <RestorantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
