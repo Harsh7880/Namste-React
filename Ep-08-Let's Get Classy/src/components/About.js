417ec53c28cd24c8a4c89321c85d200d1fe527ae
Harsh Kumar <vijayk8478@gmail.com>
2024-04-11 12:10:54 +0530

    Api Call Added in Calss based component

--- Ep-08-Let's Get Classy/src/components/About.js
+++ Ep-08-Let's Get Classy/src/components/About.js
@@ -1,20 +1,28 @@
- import User from "./User";
+import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <div>
-      <h1>this is About Us Page</h1>
-      <User 
-       name = {"Harsh Kumar"}
-       location = {"Lucknow"}
-       contact = {"vijayk8478@gmail.com"}
-      />
-      <UserClass
-      name = {"Harsh Kumar"}
-      location = {"Lucknow"}
-      contact = {"vijayk8478@gmail.com"}
-      />
-    </div>
-  );
-};
+import {Component} from 'react';
+class About extends Component {
+
+  constructor(props){
+    super(props)
+    console.log("Parent Constructor Called");
+  
+  }
+  async componentDidMount(){
+    console.log("Parent Component Did Mount Called");
+  }
+  componentDidUpdate(){
+    console.log("Parent Component did Update called");
+  }
+   render(){
+    console.log("Parent Render Called");
+    return (
+      <div>
+        <h1>this is About Us Page</h1>
+        <UserClass/>
+      </div>
+    );
+   }
+}
+
 export default About;
--- Ep-08-Let's Get Classy/src/components/UserClass.js
+++ Ep-08-Let's Get Classy/src/components/UserClass.js
@@ -3,24 +3,39 @@ class UserClass extends React.Component {
 
   constructor({props}){
    super(props);
-
    this.state = {
-    count : 0
-   }
+    userInfo : {
+      name: "Dummy User",
+      location: "Default"
+    }
+  }
+   console.log("Child Constructor Called");
   }  
- 
-
+  async componentDidMount(){
+    const data = await fetch('https://api.github.com/users/harsh7880');
+    const json = await data.json();
+    this.setState({
+       userInfo : json
+    })
+    console.log("Child Component Did Mount Called");
+  }
+  componentDidUpdate(){
+    console.log("Child Component did Update called");
+  }
+ componentWillUnmount(){
+  console.log("Component Will unmount")
+ }
   render() {
-    const {name,location,contact} = this.props; 
+    console.log("Child rendered");
+    const {name,location,contact,userName,avatar_url} = this.state.userInfo; 
     return (
       <div className="UserCard">
-        <p>Count : {this.state.count}</p>
-        <button onClick={()=>{
-          this.setState({
-            count : this.state.count + 1
-          })
-        }}>Increase Count</button>
         <h1> Class Based Component</h1>
+        <img
+        alt='profile-photo'
+        src={avatar_url}
+        />
+        <h3> Name : {userName}</h3>
         <h3>UserName: {name}</h3>
         <h3>Location : {location}</h3>
         <h3>Contact : {contact}</h3>
