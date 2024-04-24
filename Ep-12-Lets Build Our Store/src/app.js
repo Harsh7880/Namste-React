5aa7ec7f5da7876317af405a583aba0a2247f575
Harsh Kumar <vijayk8478@gmail.com>
2024-04-24 10:59:16 +0530

    Redux started

--- Ep-12-Lets Build Our Store/src/app.js
+++ Ep-12-Lets Build Our Store/src/app.js
@@ -1,4 +1,4 @@
-import React, {lazy} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header';
 import Body from './components/Body';
